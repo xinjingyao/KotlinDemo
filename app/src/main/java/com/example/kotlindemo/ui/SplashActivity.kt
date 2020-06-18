995a3294294390dc57eaf6a93f4b6579e8ee054b
threegold <yaojingxin1010@gmail.com>
2020-06-18 19:14:05 +0800

    [1.0]splash页面完成；

---
+++ app/src/main/java/com/example/kotlindemo/ui/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.example.kotlindemo.ui
+
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import com.blankj.utilcode.util.ColorUtils
+import com.blankj.utilcode.util.ImageUtils
+import com.blankj.utilcode.util.ScreenUtils
+import com.blankj.utilcode.util.SizeUtils
+import com.example.kotlindemo.R
+import com.example.kotlindemo.base.BaseActivity
+import com.example.kotlindemo.base.BasePresenter
+import com.example.kotlindemo.base.EmptyPresenter
+import com.example.kotlindemo.base.IView
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlin.math.log
+
+class SplashActivity : BaseActivity<IView, EmptyPresenter>() {
+    override fun getPresenter(): EmptyPresenter {
+        return EmptyPresenter()
+    }
+
+    override fun getLayoutId(): Int = R.layout.activity_splash
+
+    override fun initData() {
+        // 做一个圆形bitmap
+        val bitmap = ImageUtils.getBitmap(R.mipmap.ic_launcher)
+        val logo = ImageUtils.toRound(bitmap, true)
+        iv_launcher.setImageBitmap(logo)
+
+        // 渐变两秒
+        val alphaAnimation = AlphaAnimation(0.5f, 1f)
+        alphaAnimation.run {
+            duration = 2000
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationRepeat(animation: Animation?) {
+
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    LoginActivity.launch(this@SplashActivity)
+                    finish()
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+
+                }
+            })
+        }
+        cl_root.startAnimation(alphaAnimation)
+    }
+
+
+}
