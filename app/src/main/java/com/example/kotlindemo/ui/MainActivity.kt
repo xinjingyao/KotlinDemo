62f749e8b4d03bb2559928b50780f9da57b21503
threegold <yaojingxin1010@gmail.com>
2020-06-23 16:48:01 +0800

    [1.0]drawerLayout侧边栏的监听处理完毕;

--- app/src/main/java/com/example/kotlindemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kotlindemo.ui
 
 import android.content.Context
 import android.content.Intent
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.StringUtils
@@ -14,6 +16,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header.*
 
 class MainActivity : BaseActivity<IView, EmptyPresenter>() {
+
+    private var tv_username: TextView? = null
+    private var tv_id: TextView? = null
+    private var tv_level_rank: TextView? = null
+    private var iv_rank: ImageView? = null
 
     companion object{
         fun launch(context: Context) {
@@ -72,18 +79,24 @@ class MainActivity : BaseActivity<IView, EmptyPresenter>() {
     private fun initNavView() {
         nav_menu.run {
             setNavigationItemSelectedListener(onNavigationItemSelectedListener)
+            val headerView = getHeaderView(0)
+            iv_rank = headerView.findViewById(R.id.iv_rank)
+            tv_username = headerView.findViewById(R.id.tv_username)
+            tv_id = headerView.findViewById(R.id.tv_id)
+            tv_level_rank = headerView.findViewById(R.id.tv_level_rank)
         }
-        tv_username?.setOnClickListener { showToast("login") }
+        tv_username?.setOnClickListener { showToast("tv_username") }
+        iv_rank?.setOnClickListener { showToast("iv_rank") }
     }
 
     private val onNavigationItemSelectedListener = NavigationView.OnNavigationItemSelectedListener {
         when(it.itemId) {
-            R.id.nav_score -> { }
-            R.id.nav_collect -> { }
-            R.id.nav_share -> { }
-            R.id.nav_night_mode -> { }
-            R.id.nav_setting -> { }
-            R.id.nav_logout -> { }
+            R.id.nav_score -> { showToast("nav_score") }
+            R.id.nav_collect -> { showToast("nav_collect") }
+            R.id.nav_share -> { showToast("nav_share")}
+            R.id.nav_night_mode -> {showToast("nav_night_mode") }
+            R.id.nav_setting -> { showToast("nav_setting")}
+            R.id.nav_logout -> { showToast("nav_logout")}
         }
         true
     }
